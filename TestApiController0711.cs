3e10b71576d400075e15418e64c7a7106125b404
hjhfrank <71248466+hjhfrank@users.noreply.github.com>
2024-07-11 12:07:58 +0800

    Add files via upload

---
+++ TestApiController0711.cs
@@ -0,0 +1,36 @@
+﻿using Models;
+using Models.UNO;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using Org.BouncyCastle.Asn1.TeleTrust;
+
+namespace UNOTOUR.API
+{
+    public class TestApiController : ApiController
+    {
+        //private UNOContext db = new UNOContext();
+        [HttpGet]
+        [Route("API/testapi")]
+        public string testapi()
+        {
+            return "ok";
+        }
+
+        [HttpPost]
+        [Route("API/testapi1")]
+        public string testapi1([FromBody] JObject obj1)
+        {
+            string s1 = obj1["somekey"].ToString();
+            return s1;
+        }
+    }
+ }
